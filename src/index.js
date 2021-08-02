b6069a7c3012a700bcc2efd76f2f80d288db5350
Vinod Karmenghe <vinodkarmenghe@gmail.com>
2021-08-02 20:18:54 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './instagram/instaclone';
+import App from './instaclone';
 
 import reportWebVitals from './reportWebVitals';
 
